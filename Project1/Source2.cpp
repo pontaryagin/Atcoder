2f894f8c79f53f184668789e7866c5897ac094f5
pontaryagin <40906731+pontaryagin@users.noreply.github.com>
2019-04-28 18:08:32 +0900

    Revert "Merge branch 'master' of https://github.com/pontaryagin/Atcoder"
    This reverts commit 708337eecd02ad02dff4f45e371c8b1b849e35ae, reversing
    changes made to f4d0e11e086a7399f8e165c8eaf03041eec31e30.

--- Project1/Source2.cpp
+++ Project1/Source2.cpp
@@ -13,43 +13,6 @@
 int main() {
 	ll T;
 	cin >> T;
-	ll INF_ =numeric_limits<ll>::max() ;
-	rep(test, 1, T + 1) {
-		[&] {
-			ll r, b, c;
-			cin >> r >> b >> c;
-			vll m(c), s(c), p(c);
-			rep(i, 0, c) {
-				cin >> m[i] >> s[i] >> p[i];
-			}
-			ll ng = 0;
-			ll ok = INF_;
-			while (ng+1 != ok) {
-				ll mid = (ng + ok) / 2;
-				vll num(c);
-				rep(i, 0, c) {
-					num[i] = (mid - p[i]) / s[i];
-					num[i] = min(num[i], m[i]);
-				}
-				sort(all(num));
-				ll sum = accumulate(num.end() - r, num.end(),0LL);
-				if (sum >= b) {
-					ok = mid;
-				}
-				else {
-					ng = mid;
-				}
-
-			}
-
-
-			cout << "Case #" << test << ": "<< ok << endl;
-		}();
-	}
-
-	return 0;
-
-}
 	vll x;
 	cout << x[T];
 }
