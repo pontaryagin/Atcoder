7349ef54eba76e846d491b78224de4ebdf75a90d
hiro <hryk1496@gmail.com>
2019-05-22 00:58:16 +0900

    _

--- Project1/Source2.cpp
+++ Project1/Source2.cpp
@@ -22,37 +22,26 @@ int main() {
 
 	ll n;
 	cin >> n;
-	Graph g(n);
+	vll h(n), a(n);
+	rep(i, 0, n)cin >> h[i];
+	rep(i, 0, n)cin >> a[i];
 
-	rep(i, 0, n - 1) {
-		Edge e;
-		cin >> e.from >> e.to >> e.cost;
-		e.from--; e.to--;
-		g.push_undir(e);
-	}
-
-	vll color(n, -1);
-	color[0] = 0;
-	auto func = [&](Edge& e) {
-		ll x = e.to;
-		ll dist = e.cost;
-		ll col = color[e.from];
-		if (dist % 2 == 0) {
-			color[x] = col;
+	vll dp(n);
+	set<pll> me;
+	dp[0] = a[0];
+	me.insert(pll{ h[0], 0 });
+	rep(i, 1, n) {
+		auto lb = me.lower_bound(pll{ h[i],i });
+		if (lb == me.begin()) {
+			dp[i] = a[i];
 		}
 		else {
-			col ^= 1;
-			color[x] = col;
-
+			dp[i] = a[i] + dp[(--lb)->second];
 		}
-	};
-	
-	GraphDFS dfs(g);
-	dfs(func);
-	rep(i, 0, n) {
-		cout << color[i] << endl;
 	}
-
+	ll res = 0;
+	rep(i, 0, n)chmax(res, dp[i]);
+	cout << res<<endl;
 	return 0;
 
 }
