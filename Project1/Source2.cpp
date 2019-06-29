9a9f776fa92c2a4b0c311943e423804f12e3481f
pontaryagin <40906731+pontaryagin@users.noreply.github.com>
2019-06-29 18:16:10 +0900

    revert LinAlg

--- Project1/Source2.cpp
+++ Project1/Source2.cpp
@@ -1,11 +1,11 @@
 #include "MyHeader.h"
 //#include "Graph.h"
-#include "NumberTheory.h"
+//#include "NumberTheory.h"
 //#include "UnionFind.h"
 //#include "SegmentTree.h"
 //#include "Algorithm.h"
 //#include "Bit.h"
-#include "LinearAlgebra.h"
+
 
 // ============================ Header  =================================
 
@@ -16,31 +16,77 @@ int main() {
 	cout << fixed << setprecision(12);
 	
 
-	vll len(37,INF);
-	ll l, a, b, m;
-	cin >> l >> a >> b >> m;
-	ll cursz = 0;
-	rep(i, 0, l) {
-		ll sz = to_string(a + b * i).size();
-		if (sz != cursz) {
-			len[sz = i];
-			cursz = sz;
-		}
-	
-	}
+	//Int l, a, b;
+	//cin >> l >> a >> b >> MOD;
 
-	rep(r, 0, SZ(len)) {
-		ll lenth = min(len[r + 1], l) - len[r];
-		SquareMatrix<3, modint<>> sq;
-		sq.dat[0] = { POW(modint<>(10),lenth),1,1 };
-		sq.dat[1] = { 0,1,1 };
-		sq.dat[2] = { 0,0,1 };
-		sq = sq.pow(5);
-		sq =sq* sq;
+	//using T = tuple<Int, Int, Int>;
+	//vector<T> vt;
+	//Int c = a + b * (l - 1);
 
+	//auto calcLen = [](Int x) {return (Int)to_string(x).size(); };
+	//Int x = a;
+	//while (x <= c) {
+	//	Int L = (x - a) / b;
+	//	Int R = l;
+	//	if (calcLen(x) == calcLen(c)) {
+	//		vt.emplace_back(x, L, R);
+	//		break;
+	//	}
+	//	while (L + 1 < R) {
+	//		Int M = (L + R) >> 1;
+	//		if (calcLen(a + b * M) == calcLen(x)) L = M;
+	//		else R = M;
+	//	}
+	//	vt.emplace_back(x, (x - a) / b, R);
+	//	x = a + b * R;
+	//}
+	//reverse(vt.begin(), vt.end());
+	//using M = Mint<Int>;
+	//using SM = SquareMatrix<2, M>;
+	//M ans{ 0 };
+	//M mul{ 1 };
+	//for (auto st : vt) {
+	//	Int x, L, R;
+	//	tie(x, L, R) = st;
+	//	Int len = calcLen(x);
 
-	}
+	//	auto makeB =
+	//		[&](Int num) {
+	//		SM A;
+	//		A[0][0] = M(1); A[0][1] = M(1);
+	//		A[1][0] = M(0); A[1][1] = M(10).pow(len);
+	//		return M(b)* (A.pow(num))[0][1];
+	//	};
 
+	//	using P = pair<M, Int>;
+	//	auto merge =
+	//		[&](P a, P b) {
+	//		M x = a.first;
+	//		M y = b.first;
+	//		Int p = a.second;
+	//		Int q = b.second;
+
+	//		M z{ 0 };
+	//		z += x * (M(10).pow(len)).pow(q);
+	//		z += y + M(p) * makeB(q);
+
+	//		return P(z, p + q);
+	//	};
+
+	//	Int n = R - L;
+	//	P add(0, 0);
+	//	P res(x, 1);
+	//	while (n) {
+	//		if (n & 1) add = merge(add, res);
+	//		res = merge(res, res);
+	//		n >>= 1;
+	//	}
+	//	//cout<<add.first.v<<" "<<add.second<<endl;
+	//	ans += add.first * mul;
+	//	mul *= (M(10).pow(len)).pow(R - L);
+	//}
+
+	//cout << ans.v << endl;
 
 
 	return 0;
