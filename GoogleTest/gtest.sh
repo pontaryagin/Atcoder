91c32f88cff357a204f8ebfa98f10e01c5a93869
pontaryagin <40906731+pontaryagin@users.noreply.github.com>
2019-08-12 01:54:25 +0900

    fix yml

--- GoogleTest/gtest.sh
+++ GoogleTest/gtest.sh
@@ -1,11 +1,12 @@
 echo "Compiling unit tests..."
 clang++ -std=c++11 -g -Wall -Wextra -o unit_test test.cpp -lgtest -pthread 
-if [ $? ]; then
+if [  $? -ne 0  ]; then
     echo "Compile failed!"
-    exit $?
+    exit 1
 fi
 echo "Running unit tests..."
 ./unit_test -v
 result=$?
 rm -r unit_test
 echo "Unit tests completed : $result"
+exit $result
