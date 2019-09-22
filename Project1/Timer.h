7a8f761cacb33f72e3a07ea47b0693aa23ee521e
pontaryagin <40906731+pontaryagin@users.noreply.github.com>
2019-09-22 16:46:24 +0900

    焼きなまし

---
+++ Project1/Timer.h
@@ -0,0 +1,9 @@
+#include "MyHeader.h"
+#include <chrono>
+
+class Timer {
+	chrono::time_point<chrono::system_clock> start;
+public:
+	Timer() : start(chrono::system_clock::now()){}
+	double seconds() { return chrono::duration<double>(chrono::system_clock::now() - start).count(); }
+};
