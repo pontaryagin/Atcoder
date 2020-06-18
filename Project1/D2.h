5054999a9a12b6730c31c4d9c654270631a586d5
pontaryagin <40906731+pontaryagin@users.noreply.github.com>
2020-06-18 23:18:05 +0900

    Add D2 Dir

--- Project1/D2.h
+++ Project1/D2.h
@@ -2,6 +2,7 @@
 
 struct D2 {
 	enum Dir {U, D, L, R};
+	static inline const Dir Dirs[] = { U, D, L, R };
 	D2(ll h, ll w) : h(h), w(w) {};
 	bool in(ll n, D2::Dir d, ll k = 1) {
 		if ((W(n) <= k-1) && d == L)
@@ -29,3 +30,5 @@ struct D2 {
 
 	ll h, w;
 };
+
+
