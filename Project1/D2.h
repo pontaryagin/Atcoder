0b67a8e3154eb6905dfb53c70eab2c254fa9f5f8
pontaryagin <40906731+pontaryagin@users.noreply.github.com>
2020-08-25 23:45:52 +0900

    D2 enhancement

--- Project1/D2.h
+++ Project1/D2.h
@@ -4,30 +4,28 @@ struct D2 {
 	enum Dir {U, D, L, R};
 	static inline vector<Dir> Dirs = { U, D, L, R };
 	D2(ll h, ll w) : h(h), w(w) {};
-	bool in(ll n, D2::Dir d, ll k = 1) {
-		if ((W(n) <= k-1) && d == L)
-			return false;
-		if ((W(n) >= w - 1 - (k-1)) && d == R)
-			return false;
-		if ((H(n) <= k-1) && d == D)
-			return false;
-		if ((H(n) >= h - 1 - (k - 1)) && d == U)
-			return false;
-		return true;
-	};
-	ll next(Dir d, ll k = 1) {
+	bool in(ll n, Dir d, ll k = 1) {
 		switch (d)
 		{
-		case U: return w * k;
-		case D: return -w * k;
-		case L: return -k;
-		case R: return k;
+		case U: return (H(n) + k < h) && (H(n) + k >=0);
+		case D: return (H(n) - k < h) && (H(n) - k >= 0);
+		case R: return (W(n) + k >= 0) && (W(n) + k < w );
+		case L: return (W(n) - k >= 0) && (W(n) - k < w);
+		default: throw invalid_argument("not direction");
+		}
+	};
+	ll next(ll n, Dir d, ll k = 1) {
+		switch (d)
+		{
+		case U: return n + w * k;
+		case D: return n + -w * k;
+		case L: return n + -k;
+		case R: return n + k;
 		default: throw invalid_argument("not direction");
 		}
 	}
 	ll H(ll n) { return n / w; }
 	ll W(ll n) { return n % w; }
-
 	ll h, w;
 };
 
